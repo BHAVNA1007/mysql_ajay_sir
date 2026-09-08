ac88885a2e101b26f13291e335e00c503b046bb6
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-09-09 02:06:31 +0530

    foreign key tables added

--- sql_classwork/20_foreign_key/01.sql
+++ sql_classwork/20_foreign_key/01.sql
@@ -4,8 +4,69 @@ FOREGIN key:
 
 A FOREGIN key is a column combination of columns in one table that references a candidate key normaly a primary key or unique key in another table.
 
+
+
+mysql> create table department(deptid int primary key, dname varchar(20));
+Query OK, 0 rows affected (0.03 sec)
+
+mysql> create table employee(empid int primary key, empname varchar(20), deptid int, foreign key (deptid) references department(deptid));
+Query OK, 0 rows affected (0.05 sec)
+
+mysql> desc department;
++--------+-------------+------+-----+---------+-------+
+| Field  | Type        | Null | Key | Default | Extra |
++--------+-------------+------+-----+---------+-------+
+| deptid | int         | NO   | PRI | NULL    |       |
+| dname  | varchar(20) | YES  |     | NULL    |       |
++--------+-------------+------+-----+---------+-------+
+2 rows in set (0.02 sec)
+
+mysql> desc employee;
++---------+-------------+------+-----+---------+-------+
+| Field   | Type        | Null | Key | Default | Extra |
++---------+-------------+------+-----+---------+-------+
+| empid   | int         | NO   | PRI | NULL    |       |
+| empname | varchar(20) | YES  |     | NULL    |       |
+| deptid  | int         | YES  | MUL | NULL    |       |
++---------+-------------+------+-----+---------+-------+
+3 rows in set (0.00 sec)
+
+mysql> insert into department values(1, 'kiya'), (2, 'piya'), (3, 'siya');
+Query OK, 3 rows affected (0.02 sec)
+Records: 3  Duplicates: 0  Warnings: 0
+
+mysql> select * from department;
++--------+-------+
+| deptid | dname |
++--------+-------+
+|      1 | kiya  |
+|      2 | piya  |
+|      3 | siya  |
++--------+-------+
+3 rows in set (0.00 sec)
+
+mysql> insert into employee values(101, 'deepika', 1), (102, 'rashmika', 2), (103, 'katappa', 1);
+Query OK, 3 rows affected (0.01 sec)
+Records: 3  Duplicates: 0  Warnings: 0
+
+mysql> select * from employee;
++-------+----------+--------+
+| empid | empname  | deptid |
++-------+----------+--------+
+|   101 | deepika  |      1 |
+|   102 | rashmika |      2 |
+|   103 | katappa  |      1 |
++-------+----------+--------+
+3 rows in set (0.00 sec)
+
 its main perpose to maintain referencial intigrety between related tables.
 
+
+mysql> insert into employee values(104, 'bahubali', 10);
+ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`pk`.`employee`, CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`deptid`) REFERENCES `department` (`deptid`))
+
+
+the above query voilated the referencial intigrity.
 
 
 parent table and child table 
@@ -16,27 +77,111 @@ parent table and child table
 
 the table containing the refferenced key is called parent table.
 
+
+mysql> select * from department;
++--------+-------+
+| deptid | dname |
++--------+-------+
+|      1 | kiya  |
+|      2 | piya  |
+|      3 | siya  |
++--------+-------+
+3 rows in set (0.00 sec)
+
 in our example department  table  is a parent table.
 
 
 Child table:
 =============
 
-table containig the foreign key is called child table  
+table containig the foreign key is called child table  . 
+
+
+mysql> select * from employee;
++-------+----------+--------+
+| empid | empname  | deptid |
++-------+----------+--------+
+|   101 | deepika  |      1 |
+|   102 | rashmika |      2 |
+|   103 | katappa  |      1 |
++-------+----------+--------+
+3 rows in set (0.00 sec)
+
+in our example employee table is child table.
 
 
 
 example tables 
-cutomer table and order table: 
+cutomer table and orders table: 
 ==============================
-
-
-
 example tables
 
+cutomer table
+=============
+
+mysql> create table customer(cust_id int primary key, cname varchar(20), email varchar(100));
+Query OK, 0 rows affected (0.04 sec)
+
+
+mysql> desc customer;
++---------+--------------+------+-----+---------+-------+
+| Field   | Type         | Null | Key | Default | Extra |
++---------+--------------+------+-----+---------+-------+
+| cust_id | int          | NO   | PRI | NULL    |       |
+| cname   | varchar(20)  | YES  |     | NULL    |       |
+| email   | varchar(100) | YES  |     | NULL    |       |
++---------+--------------+------+-----+---------+-------+
+3 rows in set (0.00 sec)
 
 
 
+mysql> insert into customer values(101, 'depu', 'depu@gmail.com'), (102, 'rashmika', 'rashmika@gmail.com');
+Query OK, 2 rows affected (0.01 sec)
+Records: 2  Duplicates: 0  Warnings: 0
+
+
+mysql> select * from customer;
++---------+----------+--------------------+
+| cust_id | cname    | email              |
++---------+----------+--------------------+
+|     101 | depu     | depu@gmail.com     |
+|     102 | rashmika | rashmika@gmail.com |
++---------+----------+--------------------+
+2 rows in set (0.00 sec)
+
+
+
+orders table
+============
+
+mysql> create table orders(oid int primary key, order_name varchar(20), amount decimal(10,2), cust_id int, foreign key (cust_id) references customer(cust_id));
+Query OK, 0 rows affected (0.04 sec)
+
+mysql> desc orders;
++------------+---------------+------+-----+---------+-------+
+| Field      | Type          | Null | Key | Default | Extra |
++------------+---------------+------+-----+---------+-------+
+| oid        | int           | NO   | PRI | NULL    |       |
+| order_name | varchar(20)   | YES  |     | NULL    |       |
+| amount     | decimal(10,2) | YES  |     | NULL    |       |
+| cust_id    | int           | YES  | MUL | NULL    |       |
++------------+---------------+------+-----+---------+-------+
+4 rows in set (0.00 sec)
+
+
+mysql> insert into orders values(9999, 'laptop', 50000, 101), (1111, 'mobile', 3000, 101);
+
+Query OK, 2 rows affected (0.01 sec)
+Records: 2  Duplicates: 0  Warnings: 0
+
+mysql> select * from orders;
++------+------------+----------+---------+
+| oid  | order_name | amount   | cust_id |
++------+------------+----------+---------+
+| 1111 | mobile     |  3000.00 |     101 |
+| 9999 | laptop     | 50000.00 |     101 |
++------+------------+----------+---------+
+2 rows in set (0.00 sec)
 
 
 ADVANTAGES OF FK:
@@ -87,10 +232,7 @@ mysql> desc department18;
 +----------+-------------+------+-----+---------+-------+
 2 rows in set (0.04 sec)
 
-mysql> create table employee18(empid int primary key, empname varchar(20), foreign key(deptid) references  department18(deptid));
-ERROR 1072 (42000): Key column 'deptid' doesn't exist in table
-mysql> create table employee18(empid int primary key, empname varchar(20), foreign key(deptid) references  department18(deptid));
-ERROR 1072 (42000): Key column 'deptid' doesn't exist in table
+
 mysql> CREATE TABLE employee18 (
     ->     empid INT PRIMARY KEY,
     ->     empname VARCHAR(20),
@@ -291,6 +433,8 @@ mysql> CREATE TABLE tt2 (
     ->     FOREIGN KEY (id) REFERENCES tt1(id)
     -> );
 
+    ERROR 1822 (HY000): Failed to add the foreign key constraint. Missing index for constraint 'tt2_ibfk_1' in the referenced table 'tt1'
+
 
 
 NAMING A FOREGIN KEY:
@@ -339,3 +483,57 @@ Records: 0  Duplicates: 0  Warnings: 0
 
 mysql> insert into employee182 values(109, 'deepika', 'cs1234');
 Query OK, 1 row affected (0.01 sec)
+
+
+
+
+
+
+
+
+
+
+
+
+***************************************************************
+MULTIPLE FOREIGN KEY IN A TABLE:
+================================
+
+mysql> create table doctor(d_id int primary key, d_name varchar(20));
+Query OK, 0 rows affected (0.03 sec)
+
+mysql> create table pateint(p_id int primary key, p_name varchar(20));
+Query OK, 0 rows affected (0.03 sec)
+
+
+mysql> create table appointment(ap_id int primary key, d_id int,constraint fk_docter_app foreign key (d_id) references doctor (d_id), p_id int, constraint fk_pateint_app  foreign key (p_id) references pateint (p_id));
+Query OK, 0 rows affected (0.06 sec)
+
+mysql> desc  doctor;
++--------+-------------+------+-----+---------+-------+
+| Field  | Type        | Null | Key | Default | Extra |
++--------+-------------+------+-----+---------+-------+
+| d_id   | int         | NO   | PRI | NULL    |       |
+| d_name | varchar(20) | YES  |     | NULL    |       |
++--------+-------------+------+-----+---------+-------+
+2 rows in set (0.01 sec)
+
+mysql> desc  pateint;
++--------+-------------+------+-----+---------+-------+
+| Field  | Type        | Null | Key | Default | Extra |
++--------+-------------+------+-----+---------+-------+
+| p_id   | int         | NO   | PRI | NULL    |       |
+| p_name | varchar(20) | YES  |     | NULL    |       |
++--------+-------------+------+-----+---------+-------+
+2 rows in set (0.00 sec)
+
+mysql> desc appointment;
++-------+------+------+-----+---------+-------+
+| Field | Type | Null | Key | Default | Extra |
++-------+------+------+-----+---------+-------+
+| ap_id | int  | NO   | PRI | NULL    |       |
+| d_id  | int  | YES  | MUL | NULL    |       |
+| p_id  | int  | YES  | MUL | NULL    |       |
++-------+------+------+-----+---------+-------+
+3 rows in set (0.00 sec)
+
