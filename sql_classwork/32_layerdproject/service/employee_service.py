a8f73f133ba898b03f018d359d7ee73a7516031d
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-09-19 23:30:06 +0530

    employee service updated

--- sql_classwork/32_layerdproject/service/employee_service.py
+++ sql_classwork/32_layerdproject/service/employee_service.py
@@ -1,12 +1,33 @@
 from dao.employee_dao import EmployeeDao
+
+
 class EmployeeService:
+
+    def update_employee_by_id(self, id, name):
+        #update employee name by id
+        dao = EmployeeDao()
+        employee = dao.update_employee_by_id(id, name)
+        return employee
+
+    def  delete_employee_by_id(self, id):
+        #delete employee by id
+        dao = EmployeeDao()
+        employee = dao.delete_employee_by_id(id)
+        return employee
+    
+
+    def search_employee_by_id(self, id):
+        #search employee by id
+        dao = EmployeeDao()
+        employee = dao.get_employee_by_id(id)
+        return employee
+
     def display_all_employees(self):
         #code to display employee information  
         print("processing employee information")
 
         dao = EmployeeDao()
         employees = dao.get_all_employees()
-
         return employees
 
     def add_employee(self, employee):
