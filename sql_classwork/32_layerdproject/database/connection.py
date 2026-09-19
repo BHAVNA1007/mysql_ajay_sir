75ced2c68823adfd11c8c06f2f2397ff557af783
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-09-19 23:26:53 +0530

    database connection file updated

--- sql_classwork/32_layerdproject/database/connection.py
+++ sql_classwork/32_layerdproject/database/connection.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 class Database:
     def connect(self):
-       connection = mysql.connector.connect(host="localhost",port=3306,user="root",password="Bhavna@123",database="batch18")  
+       conn = mysql.connector.connect(host="localhost",port=3306,user="root",password="Bhavna@123",database="batch18")  
 
        print("database connection created")   
-       return connection 
+       return conn 
