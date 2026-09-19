952a4de75ebb5c425736bd35cc25384d521223ad
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-09-19 23:23:56 +0530

    update employeedao

--- sql_classwork/32_layerdproject/dao/employee_dao.py
+++ sql_classwork/32_layerdproject/dao/employee_dao.py
@@ -9,6 +9,70 @@ class EmployeeDao:
 
 
 
+   #update employee details
+    def update_employee_by_id(self, id, name):
+        db = Database()
+        conn = db.connect()
+        cursor = conn.cursor()
+        
+        query = "update pdemployee1 set name = %s where id = %s"
+        cursor.execute(query, (name, id))
+           
+        update_count = cursor.rowcount
+
+        conn.commit()
+         
+        if update_count > 0:
+            print("Employee updated successfully.")
+
+        else:
+            print("Employee not found.")
+
+        cursor.close()
+        conn.close()  
+        return update_count  
+
+   # delete_employee_by_id ..... do yourself     
+
+    def delete_employee_by_id(self, id):
+       db = Database()
+       conn = db.connect()
+       cursor = conn.cursor()
+
+       query = "delete from pdemployee1 where id = %s "
+       cursor.execute(query, (id,))
+
+       #row count if we want to know how many rows deleted
+      
+       deleted_count = cursor.rowcount
+
+       conn.commit()
+      
+       if deleted_count > 0 :
+           print(f"{deleted_count} employee deleted successfully.")
+       else:
+           print("employee not found")
+
+       cursor.close()
+       conn.close()
+      
+
+    def get_employee_by_id(self, id):
+        db = Database()
+        conn = db.connect()
+        cursor = conn.cursor()
+
+        query = 'select * from pdemployee1 where id = %s'
+        cursor.execute(query, (id,))
+        row = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        if row is not None:
+             employee = Employee(row[0], row[1], row[2])
+             return employee
+        return None   
+
     def get_all_employees(self):
         print("dao getting employee data")
         db = Database()
